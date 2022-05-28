ed944d23591683520bf7f4108f8f21bc66a63111
Matthew Pourroy <pourroy.matt@gmail.com>
2022-05-27 20:13:06 -0400

    Fix getGuess promp issue

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const game = {
     title: 'Guess the Number!',
-    //Prompts player to guess a number between 1 - 100
-    getGuess() {
-        return `Enter a guess between ${smallestNum} and ${biggestNum}`
-    },
     biggestNum: 100,
     smallestNum: 1,
+    //Prompts player to guess a number between 1 - 100
+    getGuess: function() {
+        prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}`)
+    },
     secretNum: null,
     //total number guessses stored in array
     prevGuesses: [],
@@ -14,3 +14,4 @@ const game = {
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     }
   }
+console.log(game.getGuess())
